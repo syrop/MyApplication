e56bcf6929c3776a6679e8621246dde6428128e4
wiktor <wiktor.nizio@gmail.com>
2019-12-24 13:08:50 +0100

    Bumped a library.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -16,13 +16,9 @@ class MainFragment : Fragment(R.layout.fr_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        val a = lifecycleScope.async {
-            cancel()
-            2
-        }
-        GlobalScope.launch {
-            println("wiktor ${a.await()}")
-            println("wiktor ale fajnie")
-        }
+        var url = "1234"
+        val hash = url.lastIndexOf("#")
+        url = url.substring(0, hash)
+        println("wiktor url: $url")
     }
 }
