7830b6453c54037127573d30febedb962ea67db9
wiktor <wiktor.nizio@gmail.com>
2019-12-24 15:54:05 +0100

    Tested canceling a job.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -3,22 +3,23 @@ package pl.org.seva.myapplication.main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.channels.Channel
-import pl.org.seva.myapplication.R
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
+import pl.org.seva.myapplication.R
 
 @ExperimentalCoroutinesApi
 class MainFragment : Fragment(R.layout.fr_main) {
 
-    private val ch = BroadcastChannel<Boolean>(Channel.CONFLATED)
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        var url = "1234"
-        val hash = url.lastIndexOf("#")
-        url = url.substring(0, hash)
-        println("wiktor url: $url")
+        val job = lifecycleScope.launch {
+            try {
+                delay(5000)
+            }
+            finally {
+                println("wiktor finally")
+            }
+        }
+        job.cancel()
     }
 }
