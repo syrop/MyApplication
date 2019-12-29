5732fee35ce334773df5d1c4f9038cce4c924786
wiktor <wiktor.nizio@gmail.com>
2019-12-29 13:55:19 +0100

    New libs.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -5,21 +5,18 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.flow.*
 import pl.org.seva.myapplication.R
 
-@ExperimentalCoroutinesApi
 class MainFragment : Fragment(R.layout.fr_main) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        val job = lifecycleScope.launch {
-            try {
-                delay(5000)
-            }
-            finally {
-                println("wiktor finally")
-            }
-        }
-        job.cancel()
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        flowOf(1, 2, 3).map { 2 }.buffer()
+
     }
 }
