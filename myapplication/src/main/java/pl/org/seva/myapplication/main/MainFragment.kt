8ce36b8222302283f3dd7f75e439e610777a7a5f
wiktor <wiktor.nizio@gmail.com>
2020-01-16 18:09:15 +0100

    New Gradle.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -1,19 +1,11 @@
 package pl.org.seva.myapplication.main
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.fr_main.*
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.invoke
 import pl.org.seva.myapplication.main.extension.nav
@@ -24,48 +16,17 @@ class MainFragment : Fragment(R.layout.fr_main) {
         next {
             nav(R.id.action_mainFragment_to_secondFragment)
         }
-        sound {
-            val mp = MediaPlayer.create(requireContext(), R.raw.knock_fingers_on_the_table)
-            mp.setOnCompletionListener {
-                it.release()
-            }
-            mp.start()
+
+        val scope = CoroutineScope(Job())
+
+        scope.launch {
+                null!!
         }
-        lifecycleScope.launch {
-            createChannel()
+
+        scope.launch {
             delay(1000)
-            sendNotification()
+            println("wiktor I am doing something")
         }
-
         val a = flow { emit(2) }
-    }
-
-    private fun sendNotification(sound: Boolean = true) {
-        val notificationBuilder = Notification.Builder(requireContext(), CHANNEL_NAME)
-        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText("Lorem ipsum")
-                .setAutoCancel(true)
-
-        val notificationManager =
-                requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
-    }
-
-    private fun createChannel() {
-        val channel = NotificationChannel(
-                CHANNEL_NAME,
-                getString(R.string.app_name),
-                NotificationManager.IMPORTANCE_DEFAULT)
-        channel.setSound(null, null)
-        channel.enableVibration(true)
-
-        val notificationManager =
-                requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-    }
-
-    companion object {
-        private const val CHANNEL_NAME = "channel"
     }
 }
