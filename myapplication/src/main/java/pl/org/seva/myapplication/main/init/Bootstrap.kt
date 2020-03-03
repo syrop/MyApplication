2d94db2f6621e7e5e3d8e9366d886ad38c392ab6
wiktor <wiktor.nizio@gmail.com>
2020-03-03 19:40:21 +0100

    Removed obsolete code.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/init/Bootstrap.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/init/Bootstrap.kt
@@ -1,11 +1,7 @@
 package pl.org.seva.myapplication.main.init
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatDelegate
-import pl.org.seva.myapplication.main.NOTIFICATION_DELETE
 import pl.org.seva.myapplication.main.createNotificationChannels
 
 
@@ -14,12 +10,5 @@ class Bootstrap {
     fun boot(ctx: Context) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         createNotificationChannels(ctx)
-        ctx.registerReceiver(NotificationBroadcastReceiver(), IntentFilter(NOTIFICATION_DELETE))
-    }
-
-    class NotificationBroadcastReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            println("wiktor notification deleted")
-        }
     }
 }
