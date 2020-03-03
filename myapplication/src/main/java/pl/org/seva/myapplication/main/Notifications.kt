38007d0db51614dfb2e28a5c36e4b93c779ae3f9
wiktor <wiktor.nizio@gmail.com>
2020-03-03 18:56:42 +0100

    New Kotlin.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/Notifications.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/Notifications.kt
@@ -3,11 +3,14 @@ package pl.org.seva.myapplication.main
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import pl.org.seva.myapplication.R
 
 const val NOTIFICATION_CHANNEL_ID = "notifications"
 const val NOTIFICATION_CHANNEL_NAME = "notifications"
+const val NOTIFICATION_DELETE = "notification_delete"
 
 fun createNotificationChannels(ctx: Context) {
     val notificationManager =
@@ -28,10 +31,18 @@ fun notify(ctx: Context, content: String) {
 
     val notificationBuilder = Notification.Builder(ctx, NOTIFICATION_CHANNEL_NAME)
 
+    val deleteIntent = Intent(NOTIFICATION_DELETE)
+    val pi = PendingIntent.getBroadcast(
+            ctx,
+            System.currentTimeMillis().toInt(),
+            deleteIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
     notificationBuilder.setSmallIcon(R.mipmap.ic_launcher)
             .setContentTitle("Title")
             .setContentText(content)
             .setAutoCancel(true)
+            .setDeleteIntent(pi)
 
     notificationManager.notify(0, notificationBuilder.build())
 }
--- myapplication/src/main/java/pl/org/seva/myapplication/main/init/Bootstrap.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/init/Bootstrap.kt
@@ -1,13 +1,25 @@
 package pl.org.seva.myapplication.main.init
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatDelegate
+import pl.org.seva.myapplication.main.NOTIFICATION_DELETE
 import pl.org.seva.myapplication.main.createNotificationChannels
+
 
 class Bootstrap {
 
     fun boot(ctx: Context) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         createNotificationChannels(ctx)
+        ctx.registerReceiver(NotificationBroadcastReceiver(), IntentFilter(NOTIFICATION_DELETE))
+    }
+
+    class NotificationBroadcastReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            println("wiktor notification deleted")
+        }
     }
 }
