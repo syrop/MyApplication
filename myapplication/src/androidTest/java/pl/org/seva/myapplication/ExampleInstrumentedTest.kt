b55ba94974fe52971e92b1185b74ed26ca23f72a
wiktor <wiktor.nizio@gmail.com>
2020-03-12 11:58:36 +0100

    Tested idling resources.

--- myapplication/src/androidTest/java/pl/org/seva/myapplication/ExampleInstrumentedTest.kt
+++ myapplication/src/androidTest/java/pl/org/seva/myapplication/ExampleInstrumentedTest.kt
@@ -1,24 +1,53 @@
 package pl.org.seva.myapplication
 
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.idling.CountingIdlingResource
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.Matchers.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import pl.org.seva.myapplication.main.MainActivity
+import kotlin.coroutines.resume
 
-import org.junit.Assert.*
+import pl.org.seva.myapplication.main.init.instance
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    private val idlingRes by instance<CountingIdlingResource>()
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java, true, true)
+
+    private suspend fun CountingIdlingResource.await() = suspendCancellableCoroutine<Unit> { cont ->
+        registerIdleTransitionCallback {
+            cont.resume(Unit)
+        }
+    }
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("pl.org.seva.myapplication", appContext.packageName)
+        runBlocking {
+            onView(withId(R.id.splash)).check(matches(isDisplayed()))
+            idlingRes.await()
+            onView(withId(R.id.splash)).check(matches(not(isDisplayed())))
+        }
     }
 }
--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.test.espresso.idling.CountingIdlingResource
 import io.reactivex.Single
 import kotlinx.android.synthetic.main.fr_main.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.rx2.await
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.invoke
 import pl.org.seva.myapplication.main.extension.nav
+import pl.org.seva.myapplication.main.init.instance
 
 class MainFragment : Fragment(R.layout.fr_main) {
+
+    private val idlingRes by instance<CountingIdlingResource>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         next {
@@ -20,8 +25,14 @@ class MainFragment : Fragment(R.layout.fr_main) {
         }
 
         lifecycleScope.launch {
-            val a = Single.just(1).await()
-
+            idlingRes.increment()
+            delay(SPLASH_DURATION)
+            splash.visibility = View.GONE
+            idlingRes.decrement()
         }
+    }
+
+    companion object {
+        private const val SPLASH_DURATION = 3500L
     }
 }
