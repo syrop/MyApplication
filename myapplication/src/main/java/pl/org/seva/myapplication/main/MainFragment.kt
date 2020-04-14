76f11be29e82ea7408860b3e586698c58b7d873d
wiktor <wiktor.nizio@gmail.com>
2020-04-14 17:31:07 +0200

    Tested connecting two receivers to one channel.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -4,35 +4,34 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import kotlinx.android.synthetic.main.fr_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import pl.org.seva.myapplication.R
+import pl.org.seva.myapplication.main.init.onEach
 
 class MainFragment : Fragment(R.layout.fr_main) {
+    private val ch = Channel<Int>(Channel.CONFLATED)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        var i = 1L
-        val time = System.currentTimeMillis()
-        lifecycleScope.launch {
-            while (true) {
-                prompt.text = i.toString()
-                withContext(Dispatchers.Default) {
-                    i++
-                }
-            }
-        }
-        lifecycleScope.launch {
-            while (true) {
-                next.text = ((System.currentTimeMillis() - time) / 1000L).toString()
-                delay(1000L)
-            }
-        }
 
-        val a = BroadcastChannel<Int>(Channel.CONFLATED)
+        lifecycleScope.launch {
+            for (counter in 1..1_000_1000) {
+                delay(1000L)
+                ch.offer(counter)
+            }
+        }
+        val job = lifecycleScope.launch {
+            ch.onEach {
+                println("wiktor $it")
+            }
+        }
+        lifecycleScope.launch {
+            delay(5000L)
+            job.cancel()
+            ch.onEach {
+                println("wiktor fun $it")
+            }
+        }
     }
 }
