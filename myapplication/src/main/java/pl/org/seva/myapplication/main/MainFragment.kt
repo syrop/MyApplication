5458658b9dce9209826524bd0d6e4bf9d5907d7e
wiktor <wiktor.nizio@gmail.com>
2020-05-14 16:34:10 +0200

    Added GSON.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -3,23 +3,18 @@ package pl.org.seva.myapplication.main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
-import kotlinx.coroutines.channels.Channel
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import pl.org.seva.myapplication.R
 
+
 class MainFragment : Fragment(R.layout.fr_main) {
-    private val ch = Channel<Int>(Channel.CONFLATED)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        Single.just {
-            Thread.sleep(1000L)
-            println("wiktor thread ${Thread.currentThread()}")
-                2
-        }.observeOn(Schedulers.computation())
-            .subscribeOn(Schedulers.io())
-            .subscribe {}
-
+        val json = "{\"threadId\":1,\"data\":{\"peerId\":1,\"aggregateId\":5134,\"senderId\":29,\"type\":\"pubsub.object-created\",\"object\":{\"peerId\":1,\"senderId\":29,\"uiId\":\"5910bc72-6e66-46db-a6a4-8747725d310a\",\"id\":5134,\"siteInfo\":null,\"content\":\"2\"},\"objectType\":\"message\"},\"sound\":\"default\",\"imageUrl\":null,\"type\":\"NativePushNotification\",\"title\":\"Hillary Clinton\",\"body\":\"2\"}"
+        println("wiktor json: $json")
+        val obj = JsonParser.parseString(json) as JsonObject
+        obj.addProperty("timestamp", System.currentTimeMillis())
+        println("wiktor obj $obj")
     }
 }
