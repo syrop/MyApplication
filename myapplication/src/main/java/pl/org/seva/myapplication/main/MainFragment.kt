08197357e2894476dab8360950b1e5989772f16d
wiktor <wiktor.nizio@gmail.com>
2020-07-30 11:13:50 +0200

    Tested logging characters.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -8,14 +8,9 @@ import pl.org.seva.myapplication.R
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
-    private var str: String? = "jadzia"
-
-    private fun popStr() = str.also {
-        str = null
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        println("wiktor pop ${popStr()}")
-        println("wiktor str $str")
+        repeat(4) {
+            println("wiktor Default-${'a' + it}")
+        }
     }
 }
