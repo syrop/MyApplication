59665d1859f257fcc0386a535bba70800933ab89
wiktor <wiktor.nizio@gmail.com>
2020-09-17 11:16:01 +0200

    Added generating an array.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -3,12 +3,28 @@ package pl.org.seva.myapplication.main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fr_main.*
 import pl.org.seva.myapplication.R
-
+import pl.org.seva.myapplication.main.extension.invoke
+import kotlin.random.Random
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
+    private val unsorted = arrayOfNulls<Int>(5)
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        generate {
+            repeat(SIZE) {
+                unsorted[it] = Random.nextInt(RANGE)
+            }
+            unsorted_view.text = unsorted.contentToString()
+        }
+    }
+
+    companion object {
+        const val RANGE = 1024
+        const val SIZE = 5
     }
 }
