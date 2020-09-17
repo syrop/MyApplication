71b8de8d0301f2ca6ed7a9f0d25476a52e1a6899
wiktor <wiktor.nizio@gmail.com>
2020-09-17 11:44:30 +0200

    Turning letters to upper case.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -5,12 +5,17 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import pl.org.seva.myapplication.R
 
-
 class MainFragment : Fragment(R.layout.fr_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        repeat(4) {
-            println("wiktor Default-${'a' + it}")
+        val product = BEGIN_WITH.toCharArray()
+        for (i in BEGIN_WITH.indices step 2) {
+            product[i] = product[i].toUpperCase()
         }
+        println("wiktor ${String(product)}")
+    }
+
+    companion object {
+        const val BEGIN_WITH = "cie gjal łang po"
     }
 }
