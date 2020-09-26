bf15aca7edb5b0c567f0056bde346820aed1af18
wiktor <wiktor.nizio@gmail.com>
2020-09-26 08:52:07 +0200

    Removed a redundant import.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/extension/LiveData.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/extension/LiveData.kt
@@ -2,7 +2,6 @@ package pl.org.seva.myapplication.main.extension
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.observe
 
 operator fun <T> LiveData<T>.invoke(owner: LifecycleOwner, observer: (T) -> Unit) =
         observe(owner) { observer(it) }
