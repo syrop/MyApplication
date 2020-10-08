6d0f07a37bcc1d729e1b7e1c8b6e13c4868584fc
wiktor <wiktor.nizio@gmail.com>
2020-10-08 14:16:15 +0200

    Added some Covid calculations.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -8,14 +8,20 @@ import pl.org.seva.myapplication.R
 class MainFragment : Fragment(R.layout.fr_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val product = BEGIN_WITH.toCharArray()
-        for (i in BEGIN_WITH.indices step 2) {
-            product[i] = product[i].toUpperCase()
-        }
-        println("wiktor ${String(product)}")
+        val oneIn = 1.0 / (PER_CENT / 100.0)
+        println("wiktor one in $oneIn")
+        val perCent = CASES_TOTAL / PER_CENT
+        println("wiktor one percent: $perCent")
+        val inHundredDays = CASES_TOTAL + 100 * CASES_PER_DAY
+        val perCentInHundredDays = inHundredDays / perCent
+        println("wiktor % in 100 days: $perCentInHundredDays")
+        val inHundredDaysOneIn = 1 / (perCentInHundredDays / 100.0)
+        println("wiktor in 100 days 1 in $inHundredDaysOneIn")
     }
 
     companion object {
-        const val BEGIN_WITH = "cie gjal łang po"
+        const val CASES_PER_DAY = 4280
+        const val CASES_TOTAL = 111_599
+        const val PER_CENT = 0.3
     }
 }
