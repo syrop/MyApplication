f060c789641aa43ccada63e791c7f8262b304ff5
wiktor <wiktor.nizio@gmail.com>
2020-10-08 14:38:35 +0200

    Added some Covid calculations.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -4,24 +4,28 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import pl.org.seva.myapplication.R
+import kotlin.math.roundToInt
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val oneIn = 1.0 / (PER_CENT / 100.0)
         println("wiktor one in $oneIn")
-        val perCent = CASES_TOTAL / PER_CENT
-        println("wiktor one percent: $perCent")
-        val inHundredDays = CASES_TOTAL + 100 * CASES_PER_DAY
-        val perCentInHundredDays = inHundredDays / perCent
-        println("wiktor % in 100 days: $perCentInHundredDays")
-        val inHundredDaysOneIn = 1 / (perCentInHundredDays / 100.0)
-        println("wiktor in 100 days 1 in $inHundredDaysOneIn")
+        val casesInPerCent = CASES_TOTAL / PER_CENT
+        println("wiktor one percent: $casesInPerCent")
+        val inXDays = CASES_TOTAL + DAYS * PER_DAY
+        val perCentInXDays = inXDays / casesInPerCent
+        val inXDaysOneIn = 1 / (perCentInXDays / 100.0)
+        println("wiktor in $DAYS days 1 in $inXDaysOneIn")
+        val daysUntilAll = (casesInPerCent * (100.0 - PER_CENT) / PER_DAY).toInt()
+        println("wiktor ${(daysUntilAll / DAYS_IN_YEAR).toInt()} years ${(daysUntilAll % DAYS_IN_YEAR).roundToInt()} days until all")
     }
 
     companion object {
-        const val CASES_PER_DAY = 4280
+        const val PER_DAY = 4280
         const val CASES_TOTAL = 111_599
         const val PER_CENT = 0.3
+        const val DAYS = 100
+        const val DAYS_IN_YEAR = 365.25
     }
 }
