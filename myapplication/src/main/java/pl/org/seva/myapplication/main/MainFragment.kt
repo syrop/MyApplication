9652d37679a1621ce5bda52f33d39aa50738b8e9
wiktor <wiktor.nizio@gmail.com>
2020-10-10 10:40:39 +0200

    Reformatted strings.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -15,10 +15,11 @@ class MainFragment : Fragment(R.layout.fr_main) {
         println("wiktor LINEAR")
         println("wiktor 1 in %.2f".format(oneIn))
         val casesInPerCent = CASES_TOTAL / PER_CENT
+        println("wiktor %% per day: %.4f".format(PER_DAY / casesInPerCent))
         val inXDays = CASES_TOTAL + DAYS * PER_DAY
         val perCentInXDays = inXDays / casesInPerCent
         val inXDaysOneIn = 1 / (perCentInXDays / 100.0)
-        println("wiktor in $DAYS days 1 in $inXDaysOneIn")
+        println("wiktor in $DAYS days 1 in %.2f".format(inXDaysOneIn))
         val daysUntilAll = (casesInPerCent * (100.0 - PER_CENT) / PER_DAY).toInt()
         println("wiktor ${daysUntilAll.toYears()} days until all")
         println("wiktor EXPONENTIAL")
@@ -29,7 +30,9 @@ class MainFragment : Fragment(R.layout.fr_main) {
         var day = 0
         while (cases < all) {
             day++
-            cases += perDay(day)
+            val perDay = perDay(day)
+            cases += perDay
+            println("wiktor day $day: $perDay")
         }
         println("wiktor ${day.toYears()}: $cases")
     }
@@ -46,12 +49,60 @@ class MainFragment : Fragment(R.layout.fr_main) {
     private fun Int.toYears() = "${(this / DAYS_IN_YEAR).toInt()} years ${(this % DAYS_IN_YEAR).roundToInt()} days"
 
     companion object {
-        const val PER_DAY = 4_739
-        const val YESTERDAY = 4_280
-        const val CASES_TOTAL = 116_338
-        const val PER_CENT = 0.31
+        const val PER_DAY = 5_300
+        const val YESTERDAY = 4_739
+        const val CASES_TOTAL = 121_638
+        const val PER_CENT = 0.32
+
         const val DAYS = 100
         const val DAYS_IN_YEAR = 365.25
-        const val DOUBLING = 3.0
+        val CASES = arrayOf(
+                5_300,
+                4_739,
+                4_280,
+                3_003,
+                2_236,
+                2_006,
+                1_934,
+                2_367,
+                2_292,
+                1_967,
+                1_552,
+                1_326,
+                1_306,
+                1_350,
+                1_584,
+                1_587,
+                1_136,
+                974,
+                711,
+                748,
+                910,
+                1_002,
+                757,
+                837,
+                600,
+                605,
+                377,
+                502,
+                603,
+                594,
+                506,
+                421,
+                400,
+                302,
+                437,
+                567,
+                691,
+                612,
+                595,
+                550,
+                502,
+                631,
+                759,
+                791,
+                887,
+                729,
+                )
     }
 }
