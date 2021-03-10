ad1ad76bfad3a292a570293cfe21917e2f6fd26b
wiktor <wiktor.nizio@gmail.com>
2021-03-10 10:34:13 +0100

    New AS.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -6,13 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onEach
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.databinding.FrMainBinding
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
     private lateinit var binding: FrMainBinding
+
+    private val flow = MutableStateFlow(0)
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -26,6 +33,8 @@ class MainFragment : Fragment(R.layout.fr_main) {
 
     @ObsoleteCoroutinesApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("krowa", "wiktor krowa")
+        lifecycleScope.launch {
+            val a = this + Dispatchers.Main
+        }
     }
 }
