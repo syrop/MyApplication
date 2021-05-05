2520732c38ca6f15a8e670bf6feb72fbe1156e75
wiktor <wiktor.nizio@gmail.com>
2021-05-05 10:41:12 +0200

    Tested flows.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -5,14 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.databinding.FrMainBinding
 
 class MainFragment : Fragment(R.layout.fr_main) {
 
     private lateinit var binding: FrMainBinding
+
+    private var flow = MutableStateFlow<Int>(0)
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -25,6 +29,13 @@ class MainFragment : Fragment(R.layout.fr_main) {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        println("wiktor kodein: ${System.identityHashCode(kodein)}")
+        lifecycleScope.launch {
+            flow.collect {
+                println("wiktor it")
+            }
+        }
+        flow.asLiveData()
+        flow.tryEmit(1)
+        println("wiktor")
     }
 }
