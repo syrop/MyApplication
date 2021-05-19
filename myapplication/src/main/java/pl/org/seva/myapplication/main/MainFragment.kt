4c0a661829b5e5657437777de4505384f2ce90d9
wiktor <wiktor.nizio@gmail.com>
2021-05-19 08:41:29 +0200

    New Gradle plugin.

--- myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
+++ myapplication/src/main/java/pl/org/seva/myapplication/main/MainFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import pl.org.seva.myapplication.R
@@ -16,26 +20,39 @@ class MainFragment : Fragment(R.layout.fr_main) {
 
     private lateinit var binding: FrMainBinding
 
-    private var flow = MutableStateFlow<Int>(0)
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
+        println("wiktor on create view")
         binding = FrMainBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        lifecycleScope.launch {
-            flow.collect {
-                println("wiktor it")
+        super.onViewCreated(view, savedInstanceState)
+
+        fun Basia.czeslaw() {
+            b = object : Ala() {
             }
         }
-        flow.asLiveData()
-        flow.tryEmit(1)
-        println("wiktor")
+
+        val a = Basia()
+        val b = Basia()
+        a.czeslaw()
+        b.czeslaw()
+        a.b.a++
+        println("wiktor a: ${a.b.a}")
+        println("wiktor b: ${b.b.a}")
     }
+}
+
+class Basia {
+    lateinit var b: Ala
+}
+
+open class Ala {
+    var a = 0
 }
